89c1867bfabe9caf697b358309dc297a1cc7a4ea
tluwaniMS <tluwani2020@gmail.com>
2022-03-09 13:41:58 +0200

    final-touches-excel-script

--- services/excel-data-extraction-service.js
+++ services/excel-data-extraction-service.js
@@ -5,7 +5,19 @@ const extractExcelData = async () => {
   const doctorsData = [];
 
   await readXlsxFile(fs.createReadStream("assets/doctors_directory.xlsx")).then((rows) => {
-    console.log(rows);
+    const rowHeaders = rows[0];
+    const totalColumns = rowHeaders.length;
+    rows.splice(0, 1);
+
+    rows.forEach((row) => {
+      let dataObject = {};
+
+      for (let i = 0; i < totalColumns; i++) {
+        dataObject[rowHeaders[i]] = row[i];
+      }
+
+      doctorsData.push(dataObject);
+    });
   });
 
   return doctorsData;
